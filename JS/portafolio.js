99872091b4ccce8e0f9216f9e2139a620435de41
David <david.romero.araez.92@gmail.com>
2017-01-25 17:06:18 +0100

    subida mi página a mi dominio, y arreglando pequeños errores

--- JS/portafolio.js
+++ JS/portafolio.js
@@ -1,5 +1,5 @@
 function f_name(){
-    
+
     var name = document.getElementById("nombre").value;
     if(name === ""){
         alert("Rellena el campo del nombre");
@@ -19,7 +19,7 @@ function comprobarEmail() {
 function validar(){
     var name = f_name();
     var email = comprobarEmail();
-    
+
     if(name && email){
         return true;
     } else{
@@ -29,13 +29,35 @@ function validar(){
 
 
 function enviarContacto(){
- 
+
     if (validar()) {
         alert("Gracias por tu mensaje");
     } else {
         return false;
     }
-    
+
 
 }
-    
+
+
+var indice = 0;
+frases = new Array();
+frases[0] = "Vamos a crear cosas increíbles juntos.";
+frases[1] = "Consigamos los propósitos deseados.";
+frases[2] = "Demostremos que nada és imposible.";
+frases[3] = "Nunca dejemos de creer.";
+
+indice = Math.random()*(frases.length);
+indice = Math.floor(indice);
+
+function rotar() {
+    if (indice == frases.length) {indice = 0;}
+    document.getElementById("rotando").innerHTML = frases[indice];
+    indice++;
+    setTimeout("rotar();",5000);
+
+} 
+
+rotar();
+
+
