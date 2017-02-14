9c7926c9452e6b4b3e5dc2e2b67017a6023eb703
David <david.romero.araez.92@gmail.com>
2017-02-14 22:47:39 +0100

    cambio en los estilos, y colocacion de los efectos de jquery

--- efectos .js
+++ efectos .js
@@ -11,20 +11,39 @@ $(document).ready(function(){
     }
 
     function animaciones(){
-    //EL TEXTO SE OCULTA Y APARECE AL HACER CLICK AL BOTON "TOGGLE"//
+        //EL TEXTO SE OCULTA Y APARECE DESDE ABAJO AL HACER CLICK AL BOTON//
         $("#hide").click(function(){
             $("#variar").animate({
                 height: 'toggle'
             });
         });
-        
-    //EL TEXTO SE DESVANECE Y APARECE AL HACER CLICK "FADEOUT FADE IN = FADETOGGLE"//
+
+        //EL TEXTO SE DESVANECE Y APARECE AL HACER CLICK "FADEOUT FADE IN = FADETOGGLE"//
         $("#hide2").click(function(){
             $("#variar2").fadeToggle(750)
         });
-    }    
-    
-    
+
+
+        // EL TEXTO APARECE Y DESAPARECE AL HACER CLICK //
+        $("#hide3").click(function(){
+            $('#variar3').toggle()
+
+        }); 
+
+       $( "#derecha" ).click(function() {
+  $( ".palabra" ).animate({ "left": "+=30px" }, "slow" );
+});
+ 
+$( "#izquierda" ).click(function(){
+  $( ".palabra" ).animate({ "left": "-=30px" }, "slow" );
+});
+        $( "#arriba" ).click(function(){
+  $( ".palabra" ).animate({ "top": "-=30px" }, "slow" );
+});
+        $( "#abajo" ).click(function(){
+  $( ".palabra" ).animate({ "top": "+=30px" }, "slow" );
+});
+    }
 
     function botones(){
         //CAMBIAR EL TEXTO DEL "BUTTON" AL HACER CLICK DE MOSTRAR/OCULTAR //
@@ -38,12 +57,9 @@ $(document).ready(function(){
             $("#hide2").text(($("#hide2").text() ==  'Fade In') ? 'Fade Out':'Fade In');
         });
 
+        $("#hide3").click(function(){
+            $("#hide3").text(($("#hide3").text() == 'Aparecer') ? 'Desaparecer':'Aparecer');
+        });
     }
 
-
-
-
-
-    
-    
 });
