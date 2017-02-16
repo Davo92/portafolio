9c451a015b51dbd6dd1e820d52b7ade30f9e8d8b
David <david.romero.araez.92@gmail.com>
2017-02-16 18:55:36 +0100

    haciendo mas efectos jquery

--- efectos .js
+++ efectos .js
@@ -30,19 +30,26 @@ $(document).ready(function(){
 
         }); 
 
-       $( "#derecha" ).click(function() {
-  $( ".palabra" ).animate({ "left": "+=30px" }, "slow" );
-});
- 
-$( "#izquierda" ).click(function(){
-  $( ".palabra" ).animate({ "left": "-=30px" }, "slow" );
-});
+        // REFERENCIA A LOS BOTONES DE DIRECCIONAMIENTO DE DESPLAZAMIENTO//
+        $( "#derecha" ).click(function() {
+            $( ".palabra" ).animate({ "left": "+=30px" }, "slow" );
+        });
+
+        $( "#izquierda" ).click(function(){
+            $( ".palabra" ).animate({ "left": "-=30px" }, "slow" );
+        });
         $( "#arriba" ).click(function(){
-  $( ".palabra" ).animate({ "top": "-=30px" }, "slow" );
-});
+            $( ".palabra" ).animate({ "top": "-=30px" }, "slow" );
+        });
         $( "#abajo" ).click(function(){
-  $( ".palabra" ).animate({ "top": "+=30px" }, "slow" );
-});
+            $( ".palabra" ).animate({ "top": "+=30px" }, "slow" );
+        });
+
+        $(document).ready(function(){
+            $("#hide5").click(function(){
+                $("#variar5").toggleClass("colorear");
+            });
+        });
     }
 
     function botones(){
@@ -51,14 +58,18 @@ $( "#izquierda" ).click(function(){
             $("#hide").text(($("#hide").text() ==  'Slide Arriba') ? 'Slide Abajo':'Slide Arriba');
         });
 
-
+        //REFERENCIA AL CAMBIO DE TEXTO DEL FADE OUT//
 
         $("#hide2").click(function(){
             $("#hide2").text(($("#hide2").text() ==  'Fade In') ? 'Fade Out':'Fade In');
         });
-
+        //REFERENCIA AL CAMBIO DE TEXTO AL BOTON DE TOGGLE//
         $("#hide3").click(function(){
             $("#hide3").text(($("#hide3").text() == 'Aparecer') ? 'Desaparecer':'Aparecer');
+        });
+
+        $("#hide5").click(function(){
+            $("#hide5").text(($("#hide5").text() == 'Quitar Clase') ? 'Añadir Clase':'Quitar Clase');
         });
     }
 
