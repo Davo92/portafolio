28d13adc5d85d31e1e56adc32062a689193b6832
David <david.romero.araez.92@gmail.com>
2017-02-25 13:37:40 +0100

    Acabado y acoplado al portfolio el proyecto de efectos y jquery

--- proyectos/efectosJquery/efectos .js
+++ proyectos/efectosJquery/efectos .js
@@ -1,6 +1,7 @@
 
 //EL TEXTO DESAPARECE, O APARECE AL HACER CLICK "TOGGLE//
 $(document).ready(function(){
+    $("#cambio7").show();
 
     funcionesIniciales();
 
@@ -20,13 +21,13 @@ $(document).ready(function(){
 
         //EL TEXTO SE DESVANECE Y APARECE AL HACER CLICK "FADEOUT FADE IN = FADETOGGLE"//
         $("#hide2").click(function(){
-            $("#variar2").fadeToggle(750)
+            $("#variar2").fadeToggle(750);
         });
 
 
         // EL TEXTO APARECE Y DESAPARECE AL HACER CLICK //
         $("#hide3").click(function(){
-            $('#variar3').toggle()
+            $('#variar3').toggle();
 
         }); 
 
@@ -45,10 +46,36 @@ $(document).ready(function(){
             $( ".palabra" ).animate({ "top": "+=30px" }, "slow" );
         });
 
-        $(document).ready(function(){
-            $("#hide5").click(function(){
-                $("#variar5").toggleClass("colorear");
+
+        $("#hide5").click(function(){
+            $("#variar5").toggleClass("colorear");
+        });
+        
+        //ELIMINAR UNA LINEA DE TEXTO EN CONCRETO//
+            $("#hide6").click(function(){
+                $("#variar6").toggle();
             });
+
+        //MOSTRAR EN DIV CON SLIDD TOGGLE Y PODERLO PARAR CUANDO QUIERAS Y REANUDARLO DESPUES//
+            $("#hide7").click(function(){
+                $("#cambio7").slideToggle(6000);
+            });
+            $("#hide7i").click(function(){
+                $("#cambio7").stop();
+            });
+        
+        //CAMBIA TEXTO AL HACER CLICK EN EL BOTON
+        $("#hide8").click(function(){
+            $("#cambio8").text(($("#cambio8").text() == 'He cambiado') ? 'Voy a cambiar':'He cambiado');
+        });
+        
+        //CAMIAR UN PARRAFO POR UN BOTON Y VICEVERSA//
+        $("#hide9").click(function(){
+            $("#cambio9").html(($("#cambio9").html() == '<button class="extra" onclick="alerta()">Nuevo Botón</button>') ? '<p>Soy un Texto</p>':'<button class="extra" onclick="alerta()">Nuevo Botón</button>');
+        });
+        
+        $("#hide10").click(function(){
+            $("#variar10").toggleClass("especial");
         });
     }
 
@@ -71,6 +98,27 @@ $(document).ready(function(){
         $("#hide5").click(function(){
             $("#hide5").text(($("#hide5").text() == 'Quitar Clase') ? 'Añadir Clase':'Quitar Clase');
         });
+
+        $("#hide6").click(function(){
+            $("#hide6").text(($("#hide6").text() == 'Añadir 2ª Línea') ? 'Quitar 2ª Línea':'Añadir 2ª Línea');
+        });
+
+        $("#hide8").click(function(){
+            $("#hide8").text(($("#hide8").text() == 'Devolver Texto') ? 'Cambiar Texto':'Devolver Texto');
+        });
+
+        $("#hide9").click(function(){
+            $("#hide9").text(($("#hide9").text() == 'Devolver Texto') ? 'Crear botón':'Devolver Texto');
+        });
+        
+        $("#hide10").click(function(){
+            $("#hide10").text(($("#hide10").text() == 'Quitar Clase') ? 'Dar Clase Especial':'Quitar Clase');
+        });
+
     }
 
 });
+
+function alerta() {
+    alert("Soy un botón con un mensaje de alerta");
+}
